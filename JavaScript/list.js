ebc63c7123e3dbfb2e7ad3628abd0b915c027634
Omkar Waghchaure <waghchaureomkar@Omkars-MacBook-Air.local>
2023-09-27 01:01:06 +0530

    list added

---
+++ JavaScript/list.js
@@ -0,0 +1,37 @@
+// 1. The Array object is used to store multiple values in a single variable.
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars);
+
+// 2. at()	Returns an indexed element of an array
+
+
+// 3. concat()	Joins arrays and returns an array with the joined arrays
+
+
+// 4. constructor	Returns the function that created the Array object's prototype
+
+
+// 5. copyWithin()	Copies array elements within the array, to and from specified positions
+
+
+// 6 .entries()	Returns a key/value pair Array Iteration Object
+
+
+// 7. every()	Checks if every element in an array pass a test
+
+
+// 8. fill()	Fill the elements in an array with a static value
+
+
+// 9. filter()	Creates a new array with every element in an array that pass a test
+
+
+// 10. find()	Returns the value of the first element in an array that pass a test
+
+
+// 11. findIndex()	Returns the index of the first element in an array that pass a test
+
+
+// 12. flat()	Concatenates sub-array elements
+
+
